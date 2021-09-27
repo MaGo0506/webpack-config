714fdc61b5bd83ee82b6383c00fb19bb2b38531a
Marko Goronjic <goronjicmarko24@gmail.com>
2021-09-27 20:08:01 +0200

    CSS, SASS, PostCSS, HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 let mode = 'development'
 
 if (process.env.NODE_ENV === 'production') {
@@ -10,17 +11,29 @@ module.exports = {
     module: {
         rules: [
         {
+            test: /\.s?css$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'postcss-loader',
+                'sass-loader'
+            ]
+        },
+        {
             test: /\.js$/,
             exclude: /node_modules/,
             use: {
-                loader: 'babel-loader',
+                loader: 'babel-loader'
             }
         },
         ]
     },
 
+    plugins: [new MiniCssExtractPlugin()],
+
     devtool: 'source-map',
     devServer: {
         static: './dist',
+        hot: true
     },
 };
